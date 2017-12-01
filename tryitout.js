22ea27618009d20346c03d6be439eaf99d223a40
Gabriel Csapo <gabecsapo@gmail.com>
2017-11-30 22:07:40 -0800

    updates link in docs page

--- tryitout.js
+++ tryitout.js
@@ -1,26 +1,26 @@
 module.exports = {
-    title: 'tap-html',
-    nav: {
-      Source: 'https://github.com/gabrielcsapo/tap-html',
-      Storybook: './storybook/index.html',
-      Example: './example/index.html'
-    },
-    body: `
-      <div class="text-center">
-        <h3 class="text-black">tap-html</h3>
-        <div class="text-black">📊 an html tap reporter</div><br/>
-        <small><pre>tape test | tap-html --out report.html</pre></small>
-        <div style="width:60%;margin: 0 auto;">
-          <img class="responsive" src="./example.png" alt="">
-        </div>
+  title: 'tap-html',
+  nav: {
+    Source: 'https://github.com/gabrielcsapo/tap-html',
+    Storybook: './storybook/index.html',
+    Example: './example/index.html'
+  },
+  body: `
+    <div class="text-center">
+      <h3 class="text-black">tap-html</h3>
+      <div class="text-black">📊 an html tap reporter</div><br/>
+      <small><pre>tape test | tap-html --out report.html</pre></small>
+      <div style="width:60%;margin: 0 auto;">
+        <img class="responsive" src="./example.png" alt="">
       </div>
-    `,
-    template: 'landing',
-    options: {
-      width: '100%'
-    },
-    output: './docs',
-    footer: `
-      <div class="text-black">Made with ☕️ by <a href="http://www.gabrielcsapo.com">@gabrielcsapo</a></div>
-    `
+    </div>
+  `,
+  template: 'landing',
+  options: {
+    width: '100%'
+  },
+  output: './docs',
+  footer: `
+    <div class="text-black">Made with ☕️ by <a href="https://www.gabrielcsapo.com">@gabrielcsapo</a></div>
+  `
 }
